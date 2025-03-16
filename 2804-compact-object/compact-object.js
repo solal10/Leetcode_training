6d165607bfd834b9f657b43531ab738a38220a54
Solal Ohana <94455719+solal10@users.noreply.github.com>
2025-03-16 15:58:43 +0200

    Time: 66 ms (90.60%) | Memory: 67.8 MB (17.13%) - LeetSync

---
+++ 2804-compact-object/compact-object.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function(obj) {
+    if (Array.isArray(obj)) {
+        return obj.map(compactObject).filter(Boolean);
+    } else if (typeof obj === 'object' && obj !== null) {
+        let result = {};
+        for (const key in obj) {
+            const value = compactObject(obj[key]); 
+            if (Boolean(value)) { 
+                result[key] = value;
+            }
+        }
+        return result;
+    }
+    return obj;
+};
