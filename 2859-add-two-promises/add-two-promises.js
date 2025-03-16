89e78f1a9108c91a8e59d5546771a7ef208e72c3
Solal Ohana <94455719+solal10@users.noreply.github.com>
2025-03-16 15:22:13 +0200

    Time: 45 ms (94.37%) | Memory: 53.8 MB (30.32%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -4,9 +4,8 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    const result1 = await promise1;
-    const result2 = await promise2;
-    return result1+result2;
+    
+    return await promise1 + await promise2;
 };
 
 /**
